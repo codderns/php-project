af6b7f92c4590c15162bdc661d450a1a12b0e464
nsgnd <53365066+nsgnd@users.noreply.github.com>
2022-01-29 23:32:04 +0300

    Add files via upload

---
+++ baglan.php
@@ -0,0 +1,14 @@
+<?php
+    try {
+        $baglan = new PDO("mysql:host=localhost;dbname=ismek;charset=utf8", "kullanici", "parola");
+    } catch (PDOException $e) {
+        echo $e->getMessage();
+    }
+
+    function isimlendir($metin="") {
+        $bul = array("ğ","ı","ü","ş","ö","ç","Ğ","İ","Ü","Ş","Ö","Ç"," ");
+        $degistir = array("g","i","u","s","o","c","G","I","U","S","O","C","_");
+        $sonuc = str_replace($bul,$degistir,$metin);
+        return mb_strtolower($sonuc,"utf8");
+    }
+?>
